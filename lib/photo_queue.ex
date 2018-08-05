b926aff80f4c06920d43e09cc41edc1125acc0a6
Bill Peregoy <bill@gnar.dog>
2018-08-04 20:11:28 -0400

    Passing unmet demand as part of state

--- lib/photo_queue.ex
+++ lib/photo_queue.ex
@@ -2,30 +2,29 @@ defmodule PhotoQueue do
   use GenStage
 
   def start_link() do
-    GenStage.start_link(PhotoQueue, :queue.new())
+    GenStage.start_link(PhotoQueue, {:queue.new(), 0})
   end
 
-  def init(queue) do
-    {:producer, queue}
+  def init(state) do
+    {:producer, state}
   end
 
   # FIXME - after queue is empty, we nom longer process new elements
-  def handle_demand(demand, queue) when demand > 0 do
-    # FIXME - assume demand is always 1 to start
+  def handle_demand(demand, {queue, unmet_demand}) when demand == 1 do
     if :queue.len(queue) > 0 do
       {{:value, item_to_process}, new_queue} = :queue.out(queue)
-      {:noreply, [item_to_process], new_queue}
+      {:noreply, [item_to_process], {new_queue, unmet_demand}}
     else
-      {:noreply, [], queue}
+      {:noreply, [], {queue, unmet_demand + demand}}
     end
   end
 
-  def handle_cast({:add, item}, queue) do
-    {:noreply, [], :queue.in(item, queue)}
+  def handle_cast({:add, item}, {queue, unmet_demand}) do
+    {:noreply, [], {:queue.in(item, queue), unmet_demand}}
   end
 
-  def handle_call(:list, _from, queue) do
-    {:reply, :queue.to_list(queue), [], queue}
+  def handle_call(:list, _from, {queue, unmet_demand}) do
+    {:reply, :queue.to_list(queue), [], {queue, unmet_demand}}
   end
 
   # Public API
